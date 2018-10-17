a102110f5cf191d1951c3497780d8c2c9972421b
xuexiang <15308367519@163.com>
2018-10-17 17:40:39 +0800

    ADD Runnable创建多线程

---
+++ src/main/java/run/MyRunnableRun.java
@@ -0,0 +1,13 @@
+package run;
+
+import com.create.MyRunnable;
+
+public class MyRunnableRun {
+
+    public static void main(String[] args) {
+        Runnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.start();
+        System.out.println("运行结束");
+    }
+}
